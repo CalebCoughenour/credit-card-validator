2fd9ba0c4a411c46d9b57e29197514e94e4e5767
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-31 10:58:25 -0700

    update validate function to check if card is valid or not valid
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 function validate(numbers) {
   if ((numbers.trim().length === 0)) {
     return "This card is not valid";
-  } else if (numbers.trim().length != 16 && numbers.trim().length != 15) {
-    return "This card is not valid";
   } else if (luhn(numbers) % 10 !== 0) {
     return "This card is not valid";
-  } else {
+  } else if ((numbers.charAt(0) === "4" || numbers.charAt(0) === "5" || numbers.charAt(0) === "6") && numbers.length === 16) {
     return "This card is valid";
+  } else if ((numbers.slice(0, 2) === "34" || numbers.slice(0, 2) === "37") && numbers.length === 15) {
+    return "This card is valid"; 
+  } else {
+    return "This card is not valid";
   }
 }
 
